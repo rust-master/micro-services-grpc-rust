27abab15395ffd81a8655346d06336a42ee9ebd9
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-08-26 12:53:29 +0500

    server and client grpc

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 // build.rs act as build script for cargo, in we configure the tonic build
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/payments.proto")?;
+    tonic_build::compile_protos("proto/payment.proto")?;
     Ok(())
 }
